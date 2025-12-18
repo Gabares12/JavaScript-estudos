ac2ad0f76eb3d902e2d9def82ce7fb2da9afa167
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-12-18 11:43:38 -0300

    Add files via upload

---
+++ JavaScript/Lista de exercícios 4/exercicios.js
@@ -0,0 +1,26 @@
+/*
+1 - 
+let nome = "Gabriel Tavares";
+alert (`ola,` + nome + `! Seja bem-vindo(a) ao curso de JavaScript! `);
+
+2 -
+let anoAtual = 2025
+let anoNascimento = 2005
+let subtracao = anoAtual - anoNascimento
+console.log(`voce tem ${subtracao} anos`)
+
+3 - 
+let cidade = prompt(`informe sua cidade`)
+let estado = prompt(`informe seu estado`)
+let pais = prompt(`informe seu pais`)
+console.log(`voce ta em ${cidade} - ${estado},${pais} `)
+
+4 -
+let temCarteira = true
+console.log(typeof temCarteira)
+*/
+let saldo = 0
+ saldo += 200
+ saque -=50
+
+ console.log(`voce tem ${saldo}`)
