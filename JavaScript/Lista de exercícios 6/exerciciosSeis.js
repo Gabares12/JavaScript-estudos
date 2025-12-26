a3af291fdacae369a38166b1f95dba4cbf92b28a
Gabriel Tavares <140209181+Gabares12@users.noreply.github.com>
2025-12-26 00:23:04 -0300

    Add files via upload

---
+++ JavaScript/Lista de exercícios 6/exerciciosSeis.js
@@ -0,0 +1,79 @@
+/*
+1 -
+let idade = 15
+idade >= 18 ? console.log('Pode beber bebida alcoólica'):
+idade <= 18 ? console.log('não pode beber alcoólica'): 
+console.log('idade inválida')
+//----------------------------------------------------------------
+2 - 
+let horaAtual = 13
+
+
+switch (horaAtual) {
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+        console.log('Bom bia')
+        break
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+        console.log('Boa tarde');
+        break
+    default:
+        console.log('Boa noite');
+
+
+}
+//----------------------------------------------------------------
+
+3 -
+let numeroQualquer = -100
+
+if (numeroQualquer >= 1 ) {
+
+    console.log('numero postivo - ',numeroQualquer)
+    
+} else if (numeroQualquer == 0) {
+    console.log('o numero é zero!', numeroQualquer)
+    
+} else {
+    console.log('numero negativo - ',numeroQualquer);
+  
+}
+//----------------------------------------------------------------
+
+4 - 
+let notaEntre = 6.5
+
+if (notaEntre >= 9) {
+
+    console.log('A');
+    
+}else if (notaEntre >= 8){
+    console.log('B');
+    
+}else if (notaEntre >= 6 ){
+    console.log('C');
+    
+}else if (notaEntre >= 4 ){
+    console.log('D');
+    
+} else if (notaEntre = 0  ){
+    console.log('E')
+}
+
+//----------------------------------------------------------------
+5 -
+let numero = 10
+
+numero % 2 === 0 ? console.log('par'):
+console.log('impar');
+*/
+
--- JavaScript/SwitchCase/Switch.js
+++ JavaScript/SwitchCase/Switch.js
@@ -3,24 +3,32 @@
 // entre 4 e 6: médio
 // entre 0 e 3: ruim
 
-const notaDoAluno = 8
+const notaDoAluno = 8.9
 
 switch (notaDoAluno) {
     case 10:
     case 9:
         console.log('Excelente')
+        break
     case 8:
     case 7:
         console.log('Bom')
+        break
     case 6:
     case 5:
     case 4:
         console.log('Mediana');
+        break
     case 3:
     case 2:
     case 1:
     case 0:
         console.log('Ruim');
+        break
+        default:
+            console.log('valor inválido');
+            break
+            
         
 
 
